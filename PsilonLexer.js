05d24ab7357acdba9d77442bd84decc4c450bb1b
chronize <sort@programmer.net>
2014-09-29 22:37:44 -0400

    support for single line comments

--- PsilonLexer.js
+++ PsilonLexer.js
@@ -37,7 +37,7 @@ var Psilon = module.exports = function() {
 };
 
 var Tokenizer = function(text) {
-	var pseudo = text.trim().match(/(\b[^\'\"]+\b)|(\'.*?\')/g),
+	var pseudo = text.trim().replace(/\/\/[^\n]*\n/, '').match(/(\b[^\'\"]+\b)|(\'.*?\')/g),
 		words = [],
 		next = 0;
 
